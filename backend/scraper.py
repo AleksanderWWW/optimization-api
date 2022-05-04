ba614b22a3a76ac8d3614b1032c604f1ad271b15
AleksanderWWW <alwojnarowicz@gmail.com>
2022-05-04 18:13:40 +0200

    add type hints; change all_dfs to all_time_series

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,5 +1,7 @@
 import datetime as dt
 
+from typing import List, Union
+
 from dataclasses import dataclass, field
 from concurrent.futures import ThreadPoolExecutor as Executor
 
@@ -19,44 +21,43 @@ class Scraper:
     Those are set using the set_tickers method of the object."""
     start: dt.date
     end: dt.date
-    all_dfs: list = field(default_factory=list)
+    all_time_series: List[pd.Series] = field(default_factory=list)
     data = pd.DataFrame()
-    tickers: list = field(default_factory=list)
+    tickers: List[str] = field(default_factory=list)
 
     @property
-    def details(self):
-        return f"Scraper(tickers_loaded: {len(self.tickers)}, data columns loaded: {len(self.all_dfs)})"
+    def details(self) -> str:
+        return f"Scraper(tickers_loaded: {len(self.tickers)}, data columns loaded: {len(self.all_time_series)})"
 
-    def set_tickers(self, tickers):
+    def set_tickers(self, tickers: List[str]) -> None:
         """Set the tickers to obtain the data for."""
         self.tickers = tickers
 
-    def get_time_series(self, ticker, provider="yahoo"):
+    def get_time_series(self, ticker: str, provider: str ="yahoo") -> None:
         """For a given ticker, the method extracts the historical prices for 
         the date range given in the object's constructor.
         The data is in the form of a pandas.DataFrame object.
         It is not returned, but appended to the the all_dfs
         object's attribute of type list."""
-        df = pdr.DataReader(ticker, provider, self.start, self.end)[
+        ts = pdr.DataReader(ticker, provider, self.start, self.end)[
             "Adj Close"]  # Only take the Adjusted Close data
-        df = df.rename(ticker)
-        self.all_dfs.append(df)  # collect the data to a list later used to create the final data set
+        ts = ts.rename(ticker)
+        self.all_time_series.append(ts)  # collect the data to a list later used to create the final data set
 
-    def scrape_data(self):
+    def scrape_data(self) -> None:
         
         with Executor(max_workers=30) as executor:
             executor.map(self.get_time_series, self.tickers)
-        self.data = self.merge_data_frames(self.all_dfs)
+        self.data = self.merge_data_frames()
         self.data = self.data.sort_index(axis=1)  # sort the column names alphabetically (they come in random order)
 
-    @staticmethod
-    def merge_data_frames(dfs) -> pd.DataFrame:
-        df = pd.concat(dfs,
+    def merge_data_frames(self) -> pd.DataFrame:
+        df = pd.concat(self.all_time_series,
                        axis=1)  # pandas.concat takes a list of dataframes and creates one big frame
         return df
 
 
-def extract_prices(tickers, years) -> pd.DataFrame:
+def extract_prices(tickers: List[str], years: Union[int, float]) -> pd.DataFrame:
     end = dt.date.today()
     start = end - dt.timedelta(days=365 * years)
 
