3ef8e007b29c8a444c2cded3d8963fb18adf951a
AleksanderWWW <alwojnarowicz@gmail.com>
2022-05-04 11:27:59 +0200

    add tests for the rest of the calculations

--- backend/test_opt_engine.py
+++ backend/test_opt_engine.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 import pandas as pd
 import numpy as np
 
@@ -21,6 +23,14 @@ class TestData:
 class TestCalculations:
     df = pd.DataFrame({"A": [.3, .4, .5],
                         "B": [.7, .1, .3]})
+    df.set_index(
+        pd.to_datetime([
+            date(2021, 5, 4),
+            date(2021, 11, 4),
+            date(2022, 5, 4)
+        ]),
+        inplace=True
+    )
     data = opt.Data(df)
     weights = np.array([.3, .7])
 
@@ -29,7 +39,9 @@ class TestCalculations:
         assert np.round(ann_sd, 2) == 23.61
 
     def test_individual_expected_returns_calculation(self):
-        assert True
+        ind_exp_rets = opt.calculate_individual_expected_returns(self.data)
+        assert (np.round(ind_exp_rets, 4) == np.array([0.25, 2.  ])).all()
 
     def test_expected_returns_calculation(self):
-        assert True
+        ind_exp_rets = opt.calculate_individual_expected_returns(self.data)
+        assert np.round(opt.calculate_expected_returns(self.weights, ind_exp_rets), 4) == 1.475
