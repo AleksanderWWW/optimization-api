2d4af57564711a0fdb1495b894d07c8585022a6d
AleksanderWWW <alwojnarowicz@gmail.com>
2022-05-08 10:05:46 +0200

    add placeholders for algo and home page routes

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+from crypt import methods
 from flask import Flask, request, jsonify
 
 from backend.scraper import extract_prices
 from core import simulate
 
 app = Flask(__name__)
+
+
+@app.route('/api/home')
+def home():
+    ...
 
 
 @app.route('/api/optimize/effFrontier', methods=['GET', 'POST'])
@@ -18,6 +24,21 @@ def optimize_portfolio_ef():
     df = extract_prices(tickers, years)
     result = simulate(df, num_portfolios)
     return jsonify(result)
+
+
+@app.route('/api/optimize/simAnnealing', methods=['GET', 'POST'])
+def optimize_portfolio_sa():
+    ...
+
+
+@app.route('/api/optimize/taboo', methods=['GET', 'POST'])
+def optimize_portfolio_taboo():
+    ...
+
+
+@app.route('/api/optimize/', methods=['GET', 'POST'])
+def optimize_portfolio_all():
+    ...
 
 
 if __name__ == '__main__':
