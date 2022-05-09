34252ccc67afab4056f1b0cf0921a1ba913e98dd
AleksanderWWW <alwojnarowicz@gmail.com>
2022-05-09 18:39:41 +0200

    apply OOP and create abstract SOlver class

--- core.py
+++ core.py
@@ -1,42 +1,75 @@
+from abc import ABC, abstractmethod
+
 import numpy as np
 import pandas as pd
 
-from backend.opt_engine import Data, calculate_volatility, calculate_individual_expected_returns, calculate_expected_returns
+from backend.opt_engine import (
+    Data, 
+    calculate_volatility, 
+    calculate_individual_expected_returns, 
+    calculate_expected_returns
+)
 
 
-def generate_portfolios(data: Data, num_portfolios: int, num_assets: int) -> pd.DataFrame:
-
-    p_ret = []  # define an empty array for portfolio returns
-    p_vol = []  # define an empty array for portfolio volatility
-    p_weights = []  # define an empty array for asset weights
-
-    # this needs to be calculated only once
-    # and then dot-producted with given 
-    # array of weights
-    ind_exp_returns = calculate_individual_expected_returns(data)
-
-    for _ in range(num_portfolios):
-        weights = np.random.random(num_assets)
-        weights = weights / np.sum(weights)
-        p_weights.append(weights)
-
-        returns = calculate_expected_returns(ind_exp_returns, weights)
-        p_ret.append(returns)
-
-        vol = calculate_volatility(data, weights) # Annual standard deviation = volatility
-        p_vol.append(vol)
-
-    df = pd.DataFrame({'Returns': p_ret, 'Volatility': p_vol})
-
-    for counter, symbol in enumerate(data.table.columns.tolist()):
-        df[symbol + ' weight'] = [w[counter] for w in p_weights]
-
-    return df
+class Solver(ABC):
+    """Abstract solver class for portfolio optimization"""
+    def __init__(self, dataframe: pd.DataFrame, rfr: int) -> None:
+        self.data = Data(dataframe)
+        self.rfr = rfr
+        
+    @abstractmethod
+    def optimize(self, *args, **kwargs):
+        ...
 
 
-def simulate(table: pd.DataFrame, num_portfolios: int, rf: int) -> dict:
-    num_assets = len(table.columns)
-    portfolios = generate_portfolios(table, num_portfolios, num_assets)
-    portfolios["Sharpe Ratio"] = (portfolios['Returns'] - rf) / portfolios['Volatility']
-    optimal_risky_portfolio = portfolios.iloc[portfolios["Sharpe Ratio"].idxmax()]
-    return optimal_risky_portfolio.to_dict()
+class EfficientFrontierSolver(Solver):
+    """Solver implementing efficient-frontier-simulation-based optimization
+    algorithm"""
+    def __init__(self, dataframe: pd.DataFrame, num_portfolios: int, rfr: int) -> None:
+        super().__init__(dataframe, rfr)
+        self.num_portfolios = num_portfolios
+
+    def generate_portfolios(self) -> pd.DataFrame:
+
+        p_ret = []  # define an empty array for portfolio returns
+        p_vol = []  # define an empty array for portfolio volatility
+        p_weights = []  # define an empty array for asset weights
+
+        # this needs to be calculated only once
+        # and then dot-producted with given 
+        # array of weights
+        ind_exp_returns = calculate_individual_expected_returns(self.data)
+
+        for _ in range(self.num_portfolios):
+            weights = np.random.random(self.data.num_assets)
+            weights = weights / np.sum(weights)
+            p_weights.append(weights)
+
+            returns = calculate_expected_returns(ind_exp_returns, weights)
+            p_ret.append(returns)
+
+            vol = calculate_volatility(self.data, weights) # Annual standard deviation = volatility
+            p_vol.append(vol)
+
+        df = pd.DataFrame({'Returns': p_ret, 'Volatility': p_vol})
+
+        for counter, symbol in enumerate(self.data.table.columns.tolist()):
+            df[symbol + ' weight'] = [w[counter] for w in p_weights]
+
+        return df
+
+    def optimize(self) -> dict:
+        portfolios = self.generate_portfolios()
+        portfolios["Sharpe Ratio"] = (portfolios['Returns'] - self.rfr) / portfolios['Volatility']
+        optimal_risky_portfolio = portfolios.iloc[portfolios["Sharpe Ratio"].idxmax()]
+        return optimal_risky_portfolio.to_dict()
+
+
+class SimulatedAnnealingSolver(Solver):
+    """Solver implementing simulated annealing optimization algorithm"""
+    pass
+
+
+class TabooSearchSolver(Solver):
+    """Solver implementing taboo search optimization algorithm"""
+    pass
