ee81e558a8e0c4d7a589cf0356f8f418885dd357
AleksanderWWW <alwojnarowicz@gmail.com>
2022-05-15 10:09:55 +0200

    add extarct_request_content function

--- utils.py
+++ utils.py
@@ -4,3 +4,28 @@ def progress_bar(progress: int, total: int) -> None:
 
     bar = '█' * int(percent) + '-' * (100 - int(percent))
     print(f"\r|{bar}| {percent: .2f}%", end='\r')
+
+
+def extract_request_content(request, num_portfolios_needed=True):
+    content = request.json
+    tickers = content["tickers"]
+
+    try:
+        years = content["years"]
+    except KeyError:
+        years = 1
+
+    try:
+        rfr = content["rfr"]
+    except KeyError:
+        rfr = 0.01
+
+    if num_portfolios_needed:
+        try:
+            num_portfolios = content["num"]
+        except KeyError:
+            num_portfolios = 10_000
+        return tickers, years, num_portfolios, rfr
+    else:
+        return tickers, years, rfr
+    
